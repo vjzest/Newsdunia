bb1b35000eedd803654453c6d6b9d7412cff8764
Vijay Maurya <126171677+vjzest@users.noreply.github.com>
2024-08-06 19:14:32 +0530

    Update Newsitems.js

--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -1,24 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Newsitems extends Component {
- 
+export class Newsitems extends React.Component {
   render() {
-   let  {title,description,imageUrl,newsUrl,author,date,source}=this.props;
+    const { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
         <div className="card">
-        <div style={{display:'flex',
-        justifyContent:'flex-end',
-        psition:'absolute',
-        right:'0'}}>
-        <span class=" badge rounded-pill bg-danger">{source}</span>
-        </div>
-          <img src={!imageUrl?"https://st.depositphotos.com/2692701/3172/i/450/depositphotos_31720091-stock-photo-single-evergreen-tree.jpg":imageUrl} className="card-img-top" alt="..." />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+          <img
+            src={
+              !imageUrl
+                ? "https://st.depositphotos.com/2692701/3172/i/450/depositphotos_31720091-stock-photo-single-evergreen-tree.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt={title || "News Image"}
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <p className="card-text"><small className="text-info">By {!author?"unknown":author} on {new Date(date).toGMTString()}</small></p>
-            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read more</a>
+            <p className="card-text">
+              <small className="text-info">
+                By {author ? author : "unknown"} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read more
+            </a>
           </div>
         </div>
       </div>
